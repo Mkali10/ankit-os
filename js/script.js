5e632a91a086b78e7f95fadb95f1f40f42e5a772
Ankit vishwa <aviatankit@gmail.com>
2026-07-10 16:10:32 +0530

    Implement counter animation on scroll
    Add an IntersectionObserver to animate counters when in view.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,43 @@
+const counters = document.querySelectorAll("[data-count]");
 
+const observer = new IntersectionObserver((entries)=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+const counter = entry.target;
+
+const target = +counter.dataset.count;
+
+let current = 0;
+
+const speed = target/80;
+
+const update=()=>{
+
+current+=speed;
+
+if(current<target){
+
+counter.innerText=Math.ceil(current);
+
+requestAnimationFrame(update);
+
+}else{
+
+counter.innerText=target+"+";
+
+}
+
+}
+
+update();
+
+}
+
+})
+
+});
+
+counters.forEach(c=>observer.observe(c));
